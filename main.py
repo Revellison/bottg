f6bd664c9bcd24fafb891e6c55fdaa9f62796881
Revellison <andrey.akimov2014@gmail.com>
2024-07-26 23:47:40 +0600

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import telebot
 
-bot = telebot.TeleBot("7268483549:AAGylutTwsfooPs8rXpho6l4yIr5Z9tDtdU")
+bot = telebot.TeleBot("BOT TOKEN")
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f"Приветствую тебя, {message.from_user.first_name} {message.from_user.last_name}, здесь происходит фарм макскоинов")
+    bot.send_message(message.chat.id, f"Приветствую тебя, {message.from_user.first_name} {message.from_user.last_name}, aboba")
 
 
 @bot.message_handler(commands=["info"])
